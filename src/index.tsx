66ad91b091ead7c54bfde142df8822416802e366
Leandro <leandrowebmaster@gmail.com>
2025-06-09 13:39:52 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -42,14 +42,12 @@
           (...args:any) => <Elements.DynView pass={{
             elementsProperties:['{}'],
 
-            styles:[
-              {
-                backgroundColor: 'white',
-                justifyContent: 'center',
-                minHeight: 22,
-                width: "100%",
-              }
-              ],
+            styles:[`{
+  width: "100%",
+  alignItems: "center",
+  justifyContent: "center",
+  padding: "20px",
+}`],
 
             functions:[()=>{}],            childrenItems:[() =><></>],
 
